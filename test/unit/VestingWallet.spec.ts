99aa4646e47b7cd4ab3539ca18b780602deeff98
wei3erHase <wei3erhase@defi.sucks>
2021-12-29 19:03:16 +0100

    test: added tests for vestedAmount (#6)
    * test: added basic unit test for vestedAmount
    
    * fix: tiny bugs in unit tests
    
    * fix: remove hardhat console
    
    * fix: remove unnecessary snapshot
    
    * feat: added ETH context to unit test
    
    * feat: added ETH address test
    
    * test: tiny fix to coverage

--- test/unit/VestingWallet.spec.ts
+++ test/unit/VestingWallet.spec.ts
@@ -19,10 +19,11 @@ describe('VestingWallet', () => {
   let snapshotId: string;
   let beneficiary: SignerWithAddress;
   let owner: SignerWithAddress;
+  let alice: SignerWithAddress;
   let dai: FakeContract<IERC20>;
 
   before(async () => {
-    [beneficiary, owner] = await ethers.getSigners();
+    [beneficiary, owner, alice] = await ethers.getSigners();
     // TODO: replace for a Mock (we use mocks in unit tests)
     vestingWalletFactory = (await ethers.getContractFactory('VestingWallet')) as VestingWallet__factory;
     vestingWallet = await vestingWalletFactory.connect(owner).deploy(NON_ZERO);
@@ -39,16 +40,72 @@ describe('VestingWallet', () => {
     expect(await vestingWallet.beneficiary()).to.equal(NON_ZERO);
   });
 
+  describe('vestedAmount', () => {
+    const startDate = 100_000_000_000; // timestamp in the future
+    const duration = 1_000_000;
+    const partialDuration = 700_000;
+
+    context('using ERC20', () => {
+      beforeEach(async () => {
+        dai.transferFrom.reset();
+        dai.transferFrom.returns(true);
+
+        await vestingWallet.connect(owner)['addBenefit(uint64,uint64,address,uint256)'](startDate, duration, DAI, VEST_AMOUNT);
+      });
+
+      it('should return 0 if vest has not yet started', async () => {
+        await evm.advanceToTimeAndBlock(startDate - 1);
+        expect(await vestingWallet['vestedAmount(address)'](DAI)).to.be.eq(0);
+      });
+
+      it('should return total bonds if vest has finalized', async () => {
+        await evm.advanceToTimeAndBlock(startDate + duration + 1);
+        expect(await vestingWallet['vestedAmount(address)'](DAI)).to.be.eq(VEST_AMOUNT);
+      });
+
+      it('should return a partial amount if vest is ongoing', async () => {
+        await evm.advanceToTimeAndBlock(startDate + partialDuration);
+        expect(await vestingWallet['vestedAmount(address)'](DAI)).to.be.eq(VEST_AMOUNT.mul(partialDuration).div(duration));
+      });
+    });
+
+    context('using ETH', () => {
+      beforeEach(async () => {
+        await vestingWallet.connect(owner)['addBenefit(uint64,uint64)'](startDate, duration, { value: VEST_AMOUNT });
+      });
+
+      it('should return 0 if vest has not yet started', async () => {
+        await evm.advanceToTimeAndBlock(startDate - 1);
+        expect(await vestingWallet['vestedAmount()']()).to.be.eq(0);
+      });
+
+      it('should return total bonds if vest has finalized', async () => {
+        await evm.advanceToTimeAndBlock(startDate + duration + 1);
+        expect(await vestingWallet['vestedAmount()']()).to.be.eq(VEST_AMOUNT);
+      });
+
+      it('should return a partial amount if vest is ongoing', async () => {
+        await evm.advanceToTimeAndBlock(startDate + partialDuration);
+        expect(await vestingWallet['vestedAmount()']()).to.be.eq(VEST_AMOUNT.mul(partialDuration).div(duration));
+      });
+
+      it('should be able to use ETH address', async () => {
+        await evm.advanceToTimeAndBlock(startDate + duration);
+        expect(await vestingWallet['vestedAmount(address)'](ETH)).to.be.eq(VEST_AMOUNT);
+      });
+    });
+  });
+
   describe('addBenefit', async () => {
     const timestamp = (await ethers.provider.getBlock('latest')).timestamp;
     const startDate = BigNumber.from(Math.floor(timestamp / 1000));
     const duration = BigNumber.from(60 * 60 * 24 * 3); // 3 months
     const releaseDate = startDate.add(duration);
 
-    context('when owner creates a ERC20 bond', async () => {
+    context('when owner creates a ERC20 bond', () => {
       beforeEach(async () => {
         await evm.snapshot.revert(snapshotId);
-        dai.transfer.reset();
+        dai.transferFrom.reset();
         dai.transferFrom.returns(true);
 
         await vestingWallet.connect(owner)['addBenefit(uint64,uint64,address,uint256)'](startDate, duration, DAI, VEST_AMOUNT);
